9aac032513252baf26ea7ce5605220f3a047a969
Matheus Gabriel <102601656+theusgab@users.noreply.github.com>
2023-01-04 15:42:43 -0300

    Add files via upload

---
+++ aula21.py
@@ -0,0 +1,8 @@
+# TIPOS BUILT-IN, DOCUMENTAÇÃO , TIPOS IMUTAVEIS, MÉTODOS DE STRING
+
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+
+string = "Matheus Gabriel"
